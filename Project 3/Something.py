0357b7c6b94f8cbfc8d5380262a916fc2c7fbd74
Steffi <s.schijf55@gmail.com>
2024-10-07 11:33:42 +0200

    some new ideas

--- Project 3/Something.py
+++ Project 3/Something.py
@@ -1,12 +1,13 @@
 import numpy as np
+from mpi4py import MPI
 
 class Appartment(): #Maybe break up into different classes. Give better name?
     def __init__(self, delta_x = 1/20):
         self.delta_x = delta_x
         self.initial_temp = 20
-        self.u01 = np.ones((int(1/delta_x), int(1/delta_x))) * self.initial_temp #the different rooms, 
-        self.u02 = np.ones((int(1/delta_x), int(2/delta_x))) * self.initial_temp
-        self.u03 = np.ones((int(1/delta_x), int(1/delta_x))) * self.initial_temp
+        self.u1 = np.ones((int(1/delta_x), int(1/delta_x))) * self.initial_temp #the different rooms, 
+        self.u2 = np.ones((int(1/delta_x), int(2/delta_x))) * self.initial_temp
+        self.u3 = np.ones((int(1/delta_x), int(1/delta_x))) * self.initial_temp
         self.NW = 15 #heat normal wall
         self.WH = 40 #heat walls with heater
         self.WW = 5  #heat wall with bit window
@@ -31,14 +32,36 @@ class Appartment(): #Maybe break up into different classes. Give better name?
         """
         pass
 
-    def neumann(self):
+    def interior(self, room):
         """
-        Implements Neumann boundary conditions
+        Input: all points in a room
+        Output: all points not next to a boundary updated
         """
         pass
 
-    def dirichlet(self):
+    def wall_boundary(self, air, heat): 
         """
-        Implements Neumann boundary conditions
+        Input: two lines of air next to the wall, heat of gthe wall
+        Output: updated air
         """
+        pass
+
+    def nuemann_dirichlet(self, dir_side, neu_side):
+        """
+        Input: sides is the air on each side of the boundary
+        Output: updated air
+        """
+        pass
+
+
+class Room():
+    def __init__(self) -> None:
+        pass
+
+    def define_boundary(self):
+        pass
+
+class Boundary():
+    def __init__(self) -> None:
+        self.H = np.zeros(20)*40
         pass
