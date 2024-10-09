32c4b2f15752ddda4fdb182b058ab30280e523be
Steffi <s.schijf55@gmail.com>
2024-10-09 14:04:38 +0200

    Neumann added (not very astetically)

--- Project_3/Appartment.py
+++ Project_3/Appartment.py
@@ -40,32 +40,40 @@ class Appartment(): #Maybe break up into different classes. Give better name?
         self.west_wall_2[self.n:] = self.u1[:,-1]
         self.east_wall_2[:self.n] = self.u3[:,0]
         self.west_wall_3 = self.u2[:self.n,-1]
-        un1 = self.heat_equation(self.u1, self.north_wall_1, self.south_wall_1, self.west_wall_1, self.east_wall_1)
-        un2 = self.heat_equation(self.u2, self.north_wall_2, self.south_wall_2, self.west_wall_2, self.east_wall_2)
-        un3 = self.heat_equation(self.u3, self.north_wall_3, self.south_wall_3, self.west_wall_3, self.east_wall_3)
+        un1 = self.heat_equation(self.u1, self.north_wall_1, self.south_wall_1, self.west_wall_1, self.east_wall_1, 1)
+        un2 = self.heat_equation(self.u2, self.north_wall_2, self.south_wall_2, self.west_wall_2, self.east_wall_2, 2)
+        un3 = self.heat_equation(self.u3, self.north_wall_3, self.south_wall_3, self.west_wall_3, self.east_wall_3, 3)
         self.u1 = un1
         self.u2 = un2
         self.u3 = un3
 
-    def heat_equation(self, u, north, south, west, east):
+    def heat_equation(self, u, north, south, west, east, room):
         un = np.zeros((len(u[:,0]), len(u[0,:])))
-
+        #Interior
         un[1:-1,1:-1] = (u[2:,1:-1] + u[:-2,1:-1] - 4*u[1:-1,1:-1] + u[1:-1,2:] + u[1:-1,:-2])
-
+        #Walls
         un[0,1:-1] = (north[1:-1] + u[1,1:-1] -4*u[0,1:-1] + u[0,0:-2] + u[0,2:])
         un[-1,1:-1] = (south[1:-1] + u[-2,1:-1] -4* u[-1,1:-1] + u[-1,0:-2] + u[-1,2:])
-
-        un[1:-1,0] = (west[1:-1] + u[1:-1,1] -4* u[1:-1,0] + u[0:-2,0] + u[2:,0])
-        un[1:-1,-1] = (east[1:-1] + u[1:-1,-2] -4* u[1:-1,-1] + u[0:-2,-1] + u[2:,-1])
-
-        un[0,0] = (north[0] + west[0] - 4*u[0,0] + u[0,1] + u[1,0])
-        un[0,-1] = (north[-1] + east[0] - 4*u[0,-1] + u[0,-2] + u[1,-1])
-        un[-1,-1] = (south[-1] + east[-1] - 4*u[-1,-1] + u[-1,-2] + u[-2,-1])
-        un[-1,0] = (south[0] + west[-1] - 4*u[-1,0] + u[-2,0] + u[-1,1])
+        if room == 3:
+            un[1:-1,0] = (u[1:-1,1] -3* u[1:-1,0] + u[0:-2,0] + u[2:,0]) #West wall
+            un[0,0] = (north[0] - 3*u[0,0] + u[0,1] + u[1,0])
+            un[-1,0] = (south[0] - 3*u[-1,0] + u[-2,0] + u[-1,1])
+        else:
+            un[1:-1,0] = (west[1:-1] + u[1:-1,1] -4* u[1:-1,0] + u[0:-2,0] + u[2:,0]) #West wall
+            un[0,0] = (north[0] + west[0] - 4*u[0,0] + u[0,1] + u[1,0])
+            un[-1,0] = (south[0] + west[-1] - 4*u[-1,0] + u[-2,0] + u[-1,1])
+        if room == 1:
+            un[1:-1,-1] = (u[1:-1,-2] -3* u[1:-1,-1] + u[0:-2,-1] + u[2:,-1]) #East wall
+            un[0,-1] = (north[-1] - 3*u[0,-1] + u[0,-2] + u[1,-1])
+            un[-1,-1] = (south[-1] - 3*u[-1,-1] + u[-1,-2] + u[-2,-1])
+        else:
+            un[1:-1,-1] = (east[1:-1] + u[1:-1,-2] -4* u[1:-1,-1] + u[0:-2,-1] + u[2:,-1])
+            un[0,-1] = (north[-1] + east[0] - 4*u[0,-1] + u[0,-2] + u[1,-1])
+            un[-1,-1] = (south[-1] + east[-1] - 4*u[-1,-1] + u[-1,-2] + u[-2,-1])
         return un * self.D + u
 
 
-AP = Appartment(delta_x = 1/50, D = 0.2)
+AP = Appartment(delta_x = 1/100, D = 0.2)
 U = np.ones((2*AP.n, 3*AP.n)) * AP.u2.min()
 U[AP.n:, :AP.n] = AP.u1
 U[:, AP.n:2*AP.n] = AP.u2
