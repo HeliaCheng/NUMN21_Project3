e5545f570021c1863e236e1da228e6623b1ecaaa
HeliaCheng <heliacheng127@gmail.com>
2024-10-14 20:53:55 +0200

    Rename folder and add sth I tried on

--- Project_3/ApartmentSetting.py
+++ Project_3/ApartmentSetting.py
@@ -1,40 +1,47 @@
 import numpy as np
 import json
 
-u_heater=40;
-u_normal=15;
-u_window=5;
+u_dict={
+"u_heater":40,
+"u_normal":15,
+"u_window":5}
+
+
+
 
 class Room:
-    def __init__(self, dimensions,boundary, adjacent_rooms=None):
-        self.dimensions = dimensions
-        self.adjacent_rooms = adjacent_rooms if adjacent_rooms else {}  # dictionary of adjacent room numbers
+    def __init__(self,rank,dim,adjacent_rooms,boundary,temperature):
+        self.rank=rank
+        self.dim=dim
+        self.adjacent_rooms =adjacent_rooms if adjacent_rooms else {}  # dictionary of adjacent room numbers
+        for key,value in temperature.items():
+             if value in u_dict:
+                setattr(self,key,u_dict[value])
+        for key,value in boundary.items():
+                setattr(self,key,value)
 
-        # self.top=top;
-        # self.bottom=bottom;
-        # self.left=left;
-        # self.right=right;
-        # self.temperature = np.full(self.dimensions, self.u_normal)
-        # self.teperature[0,:]=np.full([1,self.dimensions[1]],self.top)
-        # self.temperature[-1,:]=np.full([1,self.dimensions[1]],self.bottom)
-        # self.temperature[:,0]=np.full([1,self.dimensions[1]],self.left)
-        # self.temperature[:, -1] = np.full([1, self.dimensions[1]], self.right)
 
-def load_apartment_layout(filename='ApartmentLayout.json'):
+def get_config_by_rank(config_file, rank):
+    # Load the JSON file
+    with open(config_file, 'r') as file:
+        config_data = json.load(file)
 
-    try:
-        with open(filename, 'r') as f:
-            apart_config = json.load(f)
+    # Find the parameter set with the matching rank
+    for params in config_data['rooms']:
+        if params['rank'] == rank:
+            return params
 
-        # Ensure that the 'rooms' key is present in the configuration
-        if 'rooms' not in apart_config:
-            raise KeyError(f"Expected key 'rooms' not found in the JSON file {filename}")
+    raise ValueError(f"No parameters found for rank: {rank}")
 
-        rooms = [Room(**room_config) for room_config in apart_config['rooms']]
+def get_room_list():
+    room_list=[]
+    for rank in range(3):
+        params = get_config_by_rank("ApartmentLayout.json", rank)
+        room = Room(**params)
+        room_list.append(room)
+    return room_list
 
-        return rooms
+room_list=get_room_list()
+print(room_list[0].top_t)
+print(room_list)
 
-    except FileNotFoundError:
-        raise FileNotFoundError(f"The file {filename} was not found.")
-    except json.JSONDecodeError:
-        raise json.JSONDecodeError(f"Invalid JSON format in {filename}.")
